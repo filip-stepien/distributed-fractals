2897188a7321f4a2125dd6208c0d80205eb6b87c
Filip Stępień <filip.stepien30@gmail.com>
2026-03-20 14:34:22 +0100

    feat: add serialization class

---
+++ DistributedFractals.Communication/Serialization/ISerializer.cs
@@ -0,0 +1,7 @@
+﻿namespace DistributedFractals.Server.Serialization;
+
+public interface ISerializer
+{
+    ReadOnlyMemory<byte> Serialize<T>(T data);
+    T Deserialize<T>(ReadOnlyMemory<byte> bytes);
+}
--- DistributedFractals.Communication/Tcp/TcpMessageNodeFactory.cs
+++ DistributedFractals.Communication/Tcp/TcpMessageNodeFactory.cs
@@ -1,17 +1,18 @@
 ﻿using System.Net;
 using DistributedFractals.Server.Core;
+using DistributedFractals.Server.Serialization;
 
 namespace DistributedFractals.Server.Tcp;
 
-public class TcpMessageNodeFactory(IPAddress address, int port) : IMessageNodeFactory
+public class TcpMessageNodeFactory(IPAddress address, int port, ISerializer messageSerializer) : IMessageNodeFactory
 {
     public IMessageMasterNode CreateMaster()
     {
-        return new TcpServerNode(address, port);
+        return new TcpServerNode(address, port, messageSerializer);
     }
 
     public IMessageWorkerNode CreateWorker()
     {
-        return new TcpClientNode(address, port);
+        return new TcpClientNode(address, port, messageSerializer);
     }
 }
